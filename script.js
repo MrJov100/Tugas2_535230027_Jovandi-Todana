66af78a975ef2b594472e4cac6750a09d0284816
Jovandi <162522277+MrJov100@users.noreply.github.com>
2024-09-02 08:44:56 +0700

    js fade

--- script.js
+++ script.js
@@ -11,3 +11,39 @@ document.querySelectorAll(".navbar ul li a").forEach((anchor) => {
     });
   });
 });
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 0.5,
+  rootMargin: "0px 0px -50px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
